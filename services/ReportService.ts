37a39d336f863ab7ee1760a9d728992f76c3559c
Mohamed Benhasan <154304709+mohamedbenhasan@users.noreply.github.com>
2026-04-13 08:18:39 +0100

    Delete services directory

--- services/ReportService.ts
+++
@@ -1,7 +0,0 @@
-import { ReportData } from './report/types';
-import { ReportLayoutEngine } from './report/ReportLayoutEngine';
-
-export const generatePDFReport = async (data: ReportData) => {
-  const engine = new ReportLayoutEngine(data);
-  await engine.generate();
-};
